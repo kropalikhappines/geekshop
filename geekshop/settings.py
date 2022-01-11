47a2260cfb57f6349fb32b391c1fa24550540bce
kropalikhappines <arsik30394@gmail.com>
2022-01-11 10:30:39 +0300

    Lesson 4. Code style fix

--- geekshop/settings.py
+++ geekshop/settings.py
@@ -41,7 +41,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "mainapp",
-    'authnapp'
+    "authnapp",
 ]
 
 MIDDLEWARE = [
@@ -135,4 +135,4 @@ MEDIA_URL = "/media/"
 
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
-AUTH_USER_MODEL = 'authnapp.ShopUser'
+AUTH_USER_MODEL = "authnapp.ShopUser"
